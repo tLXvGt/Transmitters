969ac484a356efdc02afb8b359b682480253c42c
tLXvGt <tLXvGt@outlook.com>
2016-05-12 19:51:17 +0200

    Initial commit - working version

---
+++ Transmitters/Graph.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Transmitters
+{
+    public class Graph
+    {
+        public List<Node> nodes;
+        public List<Edge> edges; // af
+
+        public Graph()
+        {
+            nodes = new List<Node>();
+            edges = new List<Edge>();
+        }
+        public Graph(List<Node> inputNodes, List<Edge> inputEdges)
+        {
+            nodes = inputNodes;
+            edges = inputEdges;
+        }
+
+        public void GenerateGXL(string fileName)
+        {
+            XElement gxl = new XElement("gxl");
+
+            foreach (var item in nodes)
+            {
+                XElement node = new XElement("node");
+                XAttribute id = new XAttribute("id", item.name);
+                XAttribute xPos = new XAttribute("x",
+                    Math.Round(item.radius * Math.Cos(item.angle * Math.PI / 180), 4));
+                XAttribute yPos = new XAttribute("y",
+                    Math.Round(item.radius * Math.Sin(item.angle * Math.PI / 180), 4));
+
+                node.Add(id, xPos, yPos);
+                gxl.Add(node);
+            }
+
+            foreach (var item in edges)
+            {
+                XElement edge = new XElement("edge");
+                XAttribute id = new XAttribute("id", item.id);
+                XAttribute start = new XAttribute("start", item.start.name);
+                XAttribute end = new XAttribute("end", item.end.name);
+
+                edge.Add(id, start, end);
+                gxl.Add(edge);
+            }
+
+            gxl.Save(fileName);
+        }
+        public static Graph ReadGXL(string fileName)
+        {
+            XDocument gxlDocument = XDocument.Load(fileName);
+
+            var rawNodes = gxlDocument.Descendants().Where(n => n.Name == "node");
+            var rawEdges = gxlDocument.Descendants().Where(e => e.Name == "edge");
+
+            Console.WriteLine(gxlDocument);
+
+            var listOfReceivedNodes = new List<Node>();
+            foreach (var node in rawNodes)
+            {
+                var resolvedName = node.Attribute("id").Value.ToString();
+
+                var xCord = double.Parse(node.Attribute("x").Value.ToString(), CultureInfo.InvariantCulture);
+                var yCord = double.Parse(node.Attribute("y").Value.ToString(), CultureInfo.InvariantCulture);
+
+                var resolvedRadius = Math.Sqrt(Math.Pow(xCord, 2) + Math.Pow(yCord, 2));
+                var resolvedAngle = Math.Atan2(yCord, xCord) * 180 / Math.PI;
+
+                listOfReceivedNodes.Add(new Node(
+                    resolvedName,
+                    resolvedRadius,
+                    resolvedAngle
+                ));
+            }
+
+            var listOfReceivedEdges = new List<Edge>();
+            foreach (var edge in rawEdges)
+            {
+                var resolvedId = edge.Attribute("id").Value.ToString();
+                var resolvedStart = edge.Attribute("start").Value.ToString();
+                var resolvedEnd = edge.Attribute("end").Value.ToString();
+
+                listOfReceivedEdges.Add(new Edge(
+                    listOfReceivedNodes.Where(n => n.name == resolvedStart).Single(),
+                    listOfReceivedNodes.Where(n => n.name == resolvedEnd).Single(),
+                    int.Parse(resolvedId)
+                ));
+            }
+
+            return new Graph(listOfReceivedNodes, listOfReceivedEdges);
+        }
+    }
+
+    public class Node
+    {
+        public string name;
+        public double radius;
+        public double angle;
+        public int neighbourCount;
+        public int color;
+
+        public Node(string name, double radius, double angle)
+        {
+            this.name = name;
+            this.radius = radius;
+            this.angle = angle;
+            neighbourCount = 0;
+            color = -1;
+        }
+
+        public double CalculateDistance(Node neighbour)
+        {
+            return Math.Sqrt(Math.Pow(radius, 2) + Math.Pow(neighbour.radius, 2) - 2 * radius * neighbour.radius
+                * Math.Cos(angle * Math.PI / 180 - neighbour.angle * Math.PI / 180));
+        }
+        public bool HasColor()
+        {
+            if (color == -1)
+            {
+                return false;
+            }
+
+            return true;
+        }
+        public bool CanColor(int inputColor, List<Edge> edges)
+        {
+            if (HasColor())
+                return false;
+
+            var neighbours = GetNeighbours(edges);
+
+            foreach (var neighbour in neighbours)
+            {
+                if (neighbour.color == inputColor)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        public List<Node> GetNeighbours(List<Edge> edges)
+        {
+            var neighbours = new List<Node>();
+            foreach (var edge in edges)
+            {
+                if (edge.start.name == name)
+                {
+                    neighbours.Add(edge.end);
+                }
+                else if (edge.end.name == name)
+                {
+                    neighbours.Add(edge.start);
+                }
+            }
+
+            return neighbours;
+        }
+    }
+
+    public class Edge
+    {
+        public Node start;
+        public Node end;
+        public int id;
+
+        public static int lastEdgeId = 1;
+
+        public Edge(Node start, Node end)
+        {
+            this.start = start;
+            this.end = end;
+            id = lastEdgeId++;
+        }
+        public Edge(Node start, Node end, int id)
+        {
+            this.start = start;
+            this.end = end;
+            this.id = id;
+        }
+    }
+}
---
+++ Transmitters/MainWindow.xaml.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using Transmitters;
+
+namespace TransmittersProblem
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Node> listOfNodes = new List<Node>();
+        public List<Edge> listOfEdges = new List<Edge>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            PrepareCanvas();
+        }
+
+        private void button_Click_1(object sender, RoutedEventArgs e)
+        {
+            canvas.Children.Clear();
+            PrepareCanvas();
+
+            var graph = GenerateTransmitters(
+                int.Parse(cityRadiusTextBox.Text),
+                int.Parse(noOfTransmittersTextBox.Text),
+                int.Parse(transmitterRadiusTextBox.Text));
+            listOfNodes = graph.nodes;
+            listOfEdges = graph.edges;
+
+            foreach (var node in graph.nodes)
+            {
+                DrawColoredCircle(
+                    node.radius * Math.Cos(node.angle * Math.PI / 180),
+                    node.radius * Math.Sin(node.angle * Math.PI / 180),
+                    int.Parse(transmitterRadiusTextBox.Text
+                ));
+            }
+
+            foreach (var edge in graph.edges)
+            {
+                DrawLine(
+                    edge.start.radius * Math.Cos(edge.start.angle * Math.PI / 180),
+                    edge.start.radius * Math.Sin(edge.start.angle * Math.PI / 180),
+                    edge.end.radius * Math.Cos(edge.end.angle * Math.PI / 180),
+                    edge.end.radius * Math.Sin(edge.end.angle * Math.PI / 180)
+                );
+            }
+
+        }
+
+        public void PrepareCanvas()
+        {
+            DrawLine(0, canvas.Height / 2, 0, -canvas.Height / 2, thickness: 0.5, transparency: 40);
+            DrawLine(-canvas.Width / 2, 0, canvas.Width / 2, 0, thickness: 0.5, transparency: 40);
+            DrawColoredCircle(0, 0, int.Parse(cityRadiusTextBox.Text), thickness: 0.3);
+        }
+
+        public void DrawColoredCircle(double X, double Y, double radius,
+            int color = -1, double thickness = 1, bool hasCenter = false)
+        {
+            Ellipse circle = new Ellipse();
+            circle.Width = 2 * radius;
+            circle.Height = 2 * radius;
+            circle.Stroke = Brushes.Black;
+            circle.StrokeThickness = thickness;
+            Canvas.SetLeft(circle, canvas.Width / 2 + X - radius);
+            Canvas.SetTop(circle, canvas.Height / 2 - Y - radius);
+
+            byte transparency = 80;
+            switch (color)
+            {
+                case 1:
+                    circle.Fill = new SolidColorBrush(Color.FromArgb(transparency, 255, 0, 0));
+                    break;
+
+                case 2:
+                    circle.Fill = new SolidColorBrush(Color.FromArgb(transparency, 0, 255, 0));
+                    break;
+
+                case 3:
+                    circle.Fill = new SolidColorBrush(Color.FromArgb(transparency, 0, 0, 255));
+                    break;
+
+                case 4:
+                    circle.Fill = new SolidColorBrush(Color.FromArgb(transparency, 255, 255, 0));
+                    break;
+
+                case 5:
+                    circle.Fill = new SolidColorBrush(Color.FromArgb(transparency, 255, 0, 255));
+                    break;
+
+                case 6:
+                    circle.Fill = new SolidColorBrush(Color.FromArgb(transparency, 0, 255, 255));
+                    break;
+
+                default:
+                    break;
+            }
+
+            if (hasCenter)
+            {
+                Ellipse center = new Ellipse();
+
+                center.Width = 2;
+                center.Height = 2;
+                center.Stroke = Brushes.Black;
+                center.StrokeThickness = 2;
+
+                Canvas.SetLeft(center, canvas.Width / 2 + X - 1);
+                Canvas.SetTop(center, canvas.Height / 2 - Y - 1);
+                canvas.Children.Add(center);
+            }
+
+            canvas.Children.Add(circle);
+        }
+
+        public void DrawLine(double X1, double Y1, double X2, double Y2, double thickness = 3, byte transparency = 40)
+        {
+            Line line = new Line();
+            line.X1 = X1;
+            line.Y1 = -Y1;
+            line.X2 = X2;
+            line.Y2 = -Y2;
+            line.StrokeThickness = thickness;
+            line.Stroke = new SolidColorBrush(Color.FromArgb(transparency, 0, 0, 0));
+
+            Canvas.SetLeft(line, canvas.Width / 2);
+            Canvas.SetTop(line, canvas.Height / 2);
+
+            canvas.Children.Add(line);
+        }
+
+        public static Graph GenerateTransmitters(int _cityRadius, int _transmittersCount, int _transmitterRadius)
+        {
+            Random random = new Random();
+            var nodes = new List<Node>();
+            var edges = new List<Edge>();
+            for (int i = 0; i < _transmittersCount; i++)
+            {
+                var node = new Node("v" + (i + 1).ToString(),
+                                      random.NextDouble() * _cityRadius,
+                                      random.NextDouble() * 360);
+
+                foreach (var item in nodes)
+                {
+                    if (node.CalculateDistance(item) < 2 * _transmitterRadius)
+                    {
+                        edges.Add(new Edge(node, item));
+                        node.neighbourCount++;
+                        item.neighbourCount++;
+                    }
+                }
+
+                nodes.Add(node);
+            }
+
+            return new Graph(nodes, edges);
+        }
+
+        private void solveButton_Click(object sender, RoutedEventArgs e)
+        {
+            var sortedListOfNodes = listOfNodes.OrderByDescending(n => n.neighbourCount);
+            var currentColor = 1;
+
+            foreach (var node in sortedListOfNodes)
+            {
+                if (!node.HasColor())
+                {
+                    while (!node.CanColor(currentColor, listOfEdges))
+                    {
+                        currentColor++;
+                    }
+                    node.color = currentColor;
+
+                    DrawColoredCircle(
+                        node.radius * Math.Cos(node.angle * Math.PI / 180),
+                        node.radius * Math.Sin(node.angle * Math.PI / 180),
+                        double.Parse(transmitterRadiusTextBox.Text),
+                        currentColor
+                    );
+
+                    currentColor = 1;
+                }
+
+                var currentNeighbours = node.GetNeighbours(listOfEdges);
+                foreach (var neighbour in currentNeighbours)
+                {
+                    if (!neighbour.HasColor())
+                    {
+                        while (!neighbour.CanColor(currentColor, listOfEdges))
+                        {
+                            currentColor++;
+                        }
+                        neighbour.color = currentColor;
+
+                        DrawColoredCircle(
+                            neighbour.radius * Math.Cos(neighbour.angle * Math.PI / 180),
+                            neighbour.radius * Math.Sin(neighbour.angle * Math.PI / 180),
+                            double.Parse(transmitterRadiusTextBox.Text),
+                            currentColor
+                        );
+                        currentColor = 1;
+                    }
+
+
+                }
+
+                currentNeighbours.Clear();
+            }
+
+            /*
+            var best = listOfNodes.OrderByDescending(n => n.neighbourCount).First();
+            resultDescriptionLabel.Content = best.name + " " + best.neighbourCount;
+
+            var result = best.GetNeighbours(listOfEdges);
+            string text = "";
+            int i = 1;
+            foreach (var item in result)
+            {
+                DrawColoredCircle(
+                    item.radius * Math.Cos(item.angle * Math.PI / 180),
+                    item.radius * Math.Sin(item.angle * Math.PI / 180),
+                    double.Parse(transmitterRadiusTextBox.Text),
+                    i
+                    );
+                i++;
+                if (i>3)
+                {
+                    i = 1;
+                }
+                text += item.name + ", ";
+            }
+
+            MessageBox.Show(text);
+            */
+        }
+    }
+}
